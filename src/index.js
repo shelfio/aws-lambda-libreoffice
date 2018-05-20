ec3e16bebc792af471251b4e7d03475f2d877da6
Vlad Holubiev <golubev.vld@gmail.com>
2018-05-20 19:15:46 +0300

    refactor: get rid of class

--- src/index.js
+++ src/index.js
@@ -1,105 +1,53 @@
 const fs = require('fs');
 const path = require('path');
 const {execSync} = require('child_process');
+const defaultArgs = require('./args');
 
-class LibreOffice {
-  /**
-   * Returns a list of recommended additional LibreOffice flags.
-   *
-   * @returns {!Array<string>}
-   */
-  static get args() {
-    let result = [
-      '--disable-dev-shm-usage',
-      '--disable-notifications',
-      '--disable-offer-store-unmasked-wallet-cards',
-      '--disable-offer-upload-credit-cards',
-      '--disable-setuid-sandbox',
-      '--enable-async-dns',
-      '--enable-simple-cache-backend',
-      '--enable-tcp-fast-open',
-      '--media-cache-size=33554432',
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--no-pings',
-      '--no-sandbox',
-      '--no-zygote',
-      '--prerender-from-omnibox=disabled'
-    ];
+module.exports.defaultArgs = defaultArgs;
 
-    if (this.headless === true) {
-      result.push('--single-process');
-    } else {
-      result.push('--start-maximized');
-    }
+// see https://github.com/alixaxel/chrome-aws-lambda
+module.exports.executablePath = function() {
+  return new Promise((resolve, reject) => {
+    let input = path.join(__dirname, '..', 'bin');
+    let output = '/tmp/instdir/program/soffice';
 
-    return result;
-  }
-
-  /**
-   * Inflates the current version of LibreOffice and returns the path to the binary.
-   * If not running on AWS Lambda, `null` is returned instead.
-   *
-   * @returns {?Promise<string>}
-   */
-  static get executablePath() {
-    if (this.headless !== true) {
-      return null;
-    }
-
-    return new Promise((resolve, reject) => {
-      let input = path.join(__dirname, '..', 'bin');
-      let output = '/tmp/instdir/program/soffice';
-
-      if (fs.existsSync(output) === true) {
-        for (let file of fs.readdirSync(`/tmp`)) {
-          if (file.endsWith('.tmp') === true || file.startsWith('OSL_PIPE')) {
-            fs.unlinkSync(`/tmp/${file}`);
-          }
+    if (fs.existsSync(output) === true) {
+      for (let file of fs.readdirSync(`/tmp`)) {
+        if (file.endsWith('.tmp') === true || file.startsWith('OSL_PIPE')) {
+          fs.unlinkSync(`/tmp/${file}`);
         }
       }
+    }
 
-      for (let file of fs.readdirSync(input)) {
-        if (file.endsWith('.br') === true) {
-          input = path.join(input, file);
-        }
+    for (let file of fs.readdirSync(input)) {
+      if (file.endsWith('.br') === true) {
+        input = path.join(input, file);
       }
+    }
 
-      const source = fs.createReadStream(input);
-      const target = fs.createWriteStream('/tmp/lo.tar');
+    const source = fs.createReadStream(input);
+    const target = fs.createWriteStream('/tmp/lo.tar');
 
-      source.on('error', error => {
-        return reject(error);
-      });
-
-      target.on('error', error => {
-        return reject(error);
-      });
-
-      target.on('close', () => {
-        execSync(`cd /tmp && tar -f /tmp/lo.tar && rm -f /tmp/lo.tar`);
-
-        fs.chmod(output, '0755', error => {
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve(output);
-        });
-      });
-
-      source.pipe(require(`${__dirname}/iltorb`).decompressStream()).pipe(target);
+    source.on('error', error => {
+      return reject(error);
     });
-  }
 
-  /**
-   * Returns a boolean indicating if we are running on AWS Lambda.
-   *
-   * @returns {!boolean}
-   */
-  static get headless() {
-    return process.env.AWS_LAMBDA_FUNCTION_NAME !== undefined;
-  }
-}
+    target.on('error', error => {
+      return reject(error);
+    });
 
-module.exports = LibreOffice;
+    target.on('close', () => {
+      execSync(`cd /tmp && tar -f /tmp/lo.tar && rm -f /tmp/lo.tar`);
+
+      fs.chmod(output, '0755', error => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve(output);
+      });
+    });
+
+    source.pipe(require(`${__dirname}/iltorb`).decompressStream()).pipe(target);
+  });
+};
