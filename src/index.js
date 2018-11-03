71efe8bd399a1c40b5d9faa85a11722abc95a346
Vlad Holubiev <golubev.vld@gmail.com>
2018-11-03 22:40:31 +0200

    refactor: use aws-lambda-brotli-unpacker

--- src/index.js
+++ src/index.js
@@ -1,56 +1,15 @@
-const fs = require('fs');
+const {unpack} = require('@shelf/aws-lambda-brotli-unpacker');
 const path = require('path');
-const tar = require('tar-fs');
-const del = require('del');
 const defaultArgs = require('./args');
+const {cleanupTempFiles} = require('./cleanup');
 
 module.exports.defaultArgs = defaultArgs;
 
+const inputPath = path.join(__dirname, '..', 'bin', 'lo.tar.br');
+const outputPath = '/tmp/instdir/program/soffice';
+
 // see https://github.com/alixaxel/chrome-aws-lambda
-module.exports.getExecutablePath = function() {
-  return new Promise((resolve, reject) => {
-    let input = path.join(__dirname, '..', 'bin');
-    let output = '/tmp/instdir/program/soffice';
-
-    if (fs.existsSync(output) === true) {
-      for (let file of fs.readdirSync(`/tmp`)) {
-        if (file.endsWith('.tmp') === true || file.startsWith('OSL_PIPE')) {
-          try {
-            del.sync([`/tmp/${file}`, `/tmp/${file}/*`], {force: true});
-          } catch (error) {}
-        }
-      }
-
-      return resolve(output);
-    }
-
-    for (let file of fs.readdirSync(input)) {
-      if (file.endsWith('.br') === true) {
-        input = path.join(input, file);
-      }
-    }
-
-    const source = fs.createReadStream(input);
-    const target = tar.extract('/tmp');
-
-    source.on('error', error => {
-      return reject(error);
-    });
-
-    target.on('error', error => {
-      return reject(error);
-    });
-
-    target.on('finish', () => {
-      fs.chmod(output, '0755', error => {
-        if (error) {
-          return reject(error);
-        }
-
-        return resolve(output);
-      });
-    });
-
-    source.pipe(require(`${__dirname}/iltorb`).decompressStream()).pipe(target);
-  });
+module.exports.getExecutablePath = async function() {
+  cleanupTempFiles();
+  return unpack({inputPath, outputPath});
 };
