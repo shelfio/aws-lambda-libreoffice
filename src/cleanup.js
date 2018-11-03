f5c661f2080fe488622bd5a83c87ad693e486172
Vlad Holubiev <golubev.vld@gmail.com>
2018-11-03 22:40:31 +0200

    refactor: use aws-lambda-brotli-unpacker

---
+++ src/cleanup.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const del = require('del');
+
+module.exports.cleanupTempFiles = function() {
+  for (let file of fs.readdirSync(`/tmp`)) {
+    if (file.endsWith('.tmp') === true || file.startsWith('OSL_PIPE')) {
+      try {
+        del.sync([`/tmp/${file}`, `/tmp/${file}/*`], {force: true});
+      } catch (error) {}
+    }
+  }
+};
