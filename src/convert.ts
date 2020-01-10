8ebd39f272a47bd5290904f2cf23a9c03d1be95b
Andrii Klubuk <maaraanas@gmail.com>
2020-01-10 17:18:43 +0200

    Feat: update node to 12, convert, unpacking, validations

--- src/convert.ts
+++ src/convert.ts
@@ -1,5 +1,5 @@
-import {unpack} from '@shelf/aws-lambda-brotli-unpacker';
 import {execSync} from 'child_process';
+import {unpack} from './unpack';
 import {cleanupTempFiles} from './cleanup';
 import {getConvertedFilePath} from './logs';
 
@@ -10,30 +10,28 @@ export const defaultArgs = [
   '--view',
   '--nolockcheck',
   '--nologo',
-  '--norestore'
+  '--norestore',
+  '--nofirststartwizard'
 ];
 
 const INPUT_PATH = '/opt/lo.tar.br';
-const OUTPUT_PATH = '/tmp/instdir/program/soffice';
-
-// see https://github.com/alixaxel/chrome-aws-lambda
-export async function getExecutablePath(): Promise<string> {
-  return unpack({inputPath: INPUT_PATH, outputPath: OUTPUT_PATH});
-}
+const OUTPUT_PATH = '/tmp/instdir/program/soffice.bin';
 
 /**
  * Converts a file in /tmp to the desired file format
+ * see https://github.com/alixaxel/chrome-aws-lambda
  * @param {String} filename Name of the file to convert located in /tmp directory
  * @param {String} format File format to convert incoming file to
  * @return {Promise<String>} Absolute path to the converted file
  */
 export async function convertTo(filename: string, format: string): Promise<string> {
   cleanupTempFiles();
-
-  const binary = await getExecutablePath();
+  await unpack({inputPath: INPUT_PATH});
 
   const logs = execSync(
-    `cd /tmp && ${binary} ${defaultArgs.join(' ')} --convert-to ${format} --outdir /tmp ${filename}`
+    `cd /tmp && ${OUTPUT_PATH} ${defaultArgs.join(
+      ' '
+    )} --convert-to ${format} --outdir /tmp /tmp/${filename}`
   );
 
   execSync(`rm /tmp/${filename}`);
