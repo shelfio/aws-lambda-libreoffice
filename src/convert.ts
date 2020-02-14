9cdb6f6e76a36ff48d04c492a01a254d9fcfeb12
tadas aučiūnas <tadas.auciunas@visma.com>
2020-02-14 08:55:52 +0200

    Option to add LO extensions

--- src/convert.ts
+++ src/convert.ts
@@ -16,17 +16,28 @@ export const defaultArgs = [
 
 const INPUT_PATH = '/opt/lo.tar.br';
 const OUTPUT_PATH = '/tmp/instdir/program/soffice.bin';
+const UNOPKG_OUTPUT_PATH = '/tmp/instdir/program/unopkg.bin';
 
 /**
  * Converts a file in /tmp to the desired file format
  * @param {String} filename Name of the file to convert located in /tmp directory
  * @param {String} format File format to convert incoming file to
+ * @param {Array} extensions
  * @return {Promise<String>} Absolute path to the converted file
  */
-export async function convertTo(filename: string, format: string): Promise<string> {
+export async function convertTo(
+  filename: string,
+  format: string,
+  extensions: string[] = []
+): Promise<string> {
   let logs;
   cleanupTempFiles();
   await unpack({inputPath: INPUT_PATH});
+
+  extensions.forEach(extension => {
+    execSync(`${UNOPKG_OUTPUT_PATH} add --shared ${extension}`);
+  });
+
   const cmd = `cd /tmp && ${OUTPUT_PATH} ${defaultArgs.join(
     ' '
   )} --convert-to ${format} --outdir /tmp /tmp/${filename}`;
