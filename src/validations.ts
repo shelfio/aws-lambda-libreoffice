41112da1688dd1893ba684687c702340cb7576a3
Vlad Holubiev <golubev.vld@gmail.com>
2020-04-24 12:25:23 +0300

    feat: don't try to convert .heic files

--- src/validations.ts
+++ src/validations.ts
@@ -3,11 +3,14 @@ import isVideo from 'is-video';
 import isImage from 'is-image';
 
 export function canBeConvertedToPDF(filename: string): boolean {
+  filename = filename.toLowerCase();
+
   return (
     !isImage(filename) &&
     !isVideo(filename) &&
     !isAudio(filename) &&
     !filename.endsWith('.chm') &&
-    !filename.endsWith('.gdoc')
+    !filename.endsWith('.gdoc') &&
+    !filename.endsWith('.heic')
   );
 }
