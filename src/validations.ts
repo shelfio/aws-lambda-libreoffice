ea4ff01475d1548e81e959cccb42f7810a2a1157
Vlad Holubiev <golubev.vld@gmail.com>
2022-06-20 21:46:09 +0200

    feat: refactor supported file validation

--- src/validations.ts
+++ src/validations.ts
@@ -2,15 +2,18 @@ import isAudio from '@shelf/is-audio-filepath';
 import isVideo from 'is-video';
 import isImage from 'is-image';
 
+const UNSUPPORTED_FILE_EXTENSIONS = ['.chm', '.heic', '.gdoc', '.gsheet', '.gslides'];
+
 export function canBeConvertedToPDF(filename: string): boolean {
   filename = filename.toLowerCase();
 
-  return (
-    !isImage(filename) &&
-    !isVideo(filename) &&
-    !isAudio(filename) &&
-    !filename.endsWith('.chm') &&
-    !filename.endsWith('.gdoc') &&
-    !filename.endsWith('.heic')
+  const isFileExtensionUnsupported = UNSUPPORTED_FILE_EXTENSIONS.some(ext =>
+    filename.endsWith(ext)
   );
+
+  if (isFileExtensionUnsupported) {
+    return false;
+  }
+
+  return !isImage(filename) && !isVideo(filename) && !isAudio(filename);
 }
