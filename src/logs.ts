f144eeedb2ec27ccd9010c4528b2a613607b202b
Vitaliy Shcherban <scherbanvitali@gmail.com>
2022-08-11 16:16:03 +0300

    feat: throw error with log string when trying to getCovertedFilePath

--- src/logs.ts
+++ src/logs.ts
@@ -1,3 +1,10 @@
 export function getConvertedFilePath(logs: string): string {
-  return logs.match(/\/tmp\/.+->\s(\/tmp\/.+) using/)[1];
+  try {
+    return logs.match(/\/tmp\/.+->\s(\/tmp\/.+) using/)[1];
+  } catch (e) {
+    const ErrorWithExtendedMessage: Error = new Error(e);
+    ErrorWithExtendedMessage.message += `;\tTried to parse string: "${logs}"`;
+
+    throw ErrorWithExtendedMessage;
+  }
 }
