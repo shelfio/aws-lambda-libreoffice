a025ec5d9f2336f20d30c3a614428d5c354b38c8
Andrii Bakanov <andrii.bakanov@shelf.io>
2025-01-03 11:55:25 +0100

    test: LOAPI-15 adjusted file names

--- src/validations.test.ts
+++ src/validations.test.ts
@@ -2,23 +2,23 @@ import {canBeConvertedToPDF} from './validations';
 
 it.each`
   filename
-  ${'image.docx'}
-  ${'image.pdf'}
+  ${'document.docx'}
+  ${'document.pdf'}
 `('should return true for supported filename $filename', ({filename}) => {
   expect(canBeConvertedToPDF(filename)).toEqual(true);
 });
 
 it.each`
   filename
-  ${'image.mpp'}
-  ${'image.msg'}
+  ${'project.mpp'}
+  ${'email.msg'}
   ${'image.jpg'}
-  ${'image.mp4'}
-  ${'image.mp3'}
-  ${'image.wav'}
-  ${'image.chm'}
-  ${'image.gdoc'}
-  ${'image.dwg'}
+  ${'video.mp4'}
+  ${'audio.mp3'}
+  ${'sound.wav'}
+  ${'help.chm'}
+  ${'google-doc.gdoc'}
+  ${'drawing.dwg'}
 `('should return false for unsupported filename $filename', ({filename}) => {
   expect(canBeConvertedToPDF(filename)).toEqual(false);
 });
